139f99774a7324c1baa1e037e4953239a2fa8f4a
Audrey Serene <audrey.serene@gmail.com>
2023-12-21 15:56:04 -0600

    Fix Github & LinkedIn URLs

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -9,8 +9,8 @@ function Contact() {
                 </header>
                 <Menu className='w-fit' model={[
                     { label: 'audrey.serene@gmail.com', url: 'mailto:audrey.serene@gmail.com', icon: 'pi pi-envelope' },
-                    { label: 'LinkedIn', url: 'https://linkedin.com/in/audreyerserene', icon: 'pi pi-linkedin' },
-                    { label: 'Github', url: 'https:/github.com/audse', icon: 'pi pi-github' }
+                    { label: 'LinkedIn', url: 'https://linkedin.com/in/audreyserene', icon: 'pi pi-linkedin' },
+                    { label: 'Github', url: 'https://github.com/audse', icon: 'pi pi-github' }
                 ]} />
             </div>
         </footer>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -11,16 +11,20 @@ function Nav() {
         <Menubar
             className='m-2'
             model={[
-                { label: 'Work', command: () => goToId('work') },
-                { label: 'Projects', command: () => goToId('projects') },
-                { label: 'About', command: () => goToId('about') },
-                { label: 'Contact', command: () => goToId('contact') },
+                { label: 'Work', url: '#work' },
+                { label: 'Projects', url: '#projects' },
+                { label: 'About', url: '#about' },
+                { label: 'Contact', url: '#contact' },
             ]}
             start={ <span className='block ps-4 pe-12 text-muted'>Audrey Serene</span> }
             end={
                 <>
-                <Button icon='pi pi-linkedin' text onClick={ () => window.open('https://linkedin.com/in/audreyerserene', 'blank') } />
-                <Button icon='pi pi-github' text onClick={ () => window.open('https:/github.com/audse', 'blank') } />
+                    <a href='https://linkedin.com/in/audreyserene' className='p-button p-component p-button-icon-only p-button-text me-2' target='_blank'>
+                        <span className='p-button-icon p-c pi pi-linkedin' />
+                    </a>
+                    <a href='https://github.com/audse' className='p-button p-component p-button-icon-only p-button-text' target='_blank'>
+                        <span className='p-button-icon p-c pi pi-github' />
+                    </a>
                 </>
             } />
         <ScrollTop />
